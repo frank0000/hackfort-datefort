46a6d297edf2e550bc75b9aa63a68d5ed80fd7b9
unknown <nhoffman@IT16704.BOISE.LOCAL>
2016-02-29 11:46:23 -0700

    New version of Datefort, with revised data sources, mapping assets, etc.

--- src/main/resources/static/app/event.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "/events/getEvent"
-    }).then(function(data) {
-       $('.event-activity').append(data.activityName);
-       $('.event-location').append(data.locationName);
-    });
-});
